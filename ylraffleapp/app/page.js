0722d8ce81af1c5010875080be6dae6d385d86de
oschultz47 <oschultz47@tamu.edu>
2024-05-29 15:27:15 -0500

    Navbar on home page

--- ylraffleapp/app/page.js
+++ ylraffleapp/app/page.js
@@ -3,15 +3,37 @@
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
-import { signOut } from 'aws-amplify/auth';
 import awsconfig from '../src/aws-exports';
+import { getCurrentUser } from 'aws-amplify/auth';
+import * as React from 'react';
+import ButtonAppBar from './appBar';
 
 Amplify.configure(awsconfig);
 
 function Home() {
+  
+  const [user, setUser] = React.useState({});
+
+  React.useEffect(() => {
+    try {
+      getCurrentUser().then((user) => {
+        setUser(user);
+      });
+    }
+    catch {
+      console.log('No user');
+    }
+  }, []);
+
+  if (!user.signInDetails) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <button onClick={() => signOut()}>Sign out</button>
+      <ButtonAppBar />
+      {console.log("User: ", user)}
+      <p>Email: {user.signInDetails.loginId} </p>
     </div>
   );
 }
