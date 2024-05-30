1624210edc020684c52c27e77a1ffb4e1b3a9b7e
oschultz47 <oschultz47@tamu.edu>
2024-05-30 16:00:58 -0500

    connected to database

--- ylraffleapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ ylraffleapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,16 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apiae2c3e0f": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    },
+    "ylraffleapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "ylraffleappfdc5f488": {
       "AppClientID": "string",
@@ -8,6 +20,22 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "ylraffleapp55c1f14c": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "ylraffleapp88b5f6df": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
--- ylraffleapp/app/page.js
+++ ylraffleapp/app/page.js
@@ -5,14 +5,18 @@ import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
 import awsconfig from '../src/aws-exports';
 import { getCurrentUser } from 'aws-amplify/auth';
+import { get } from 'aws-amplify/api';
 import * as React from 'react';
 import ButtonAppBar from './appBar';
+
 
 Amplify.configure(awsconfig);
 
 function Home() {
   
   const [user, setUser] = React.useState({});
+  const [tableData, setTableData] = React.useState([]);
+
 
   React.useEffect(() => {
     try {
@@ -25,6 +29,44 @@ function Home() {
     }
   }, []);
 
+  React.useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const restOperation = get({
+          apiName: 'ylraffleapi',
+          path: '/raffle'
+        });
+        const response = await restOperation.response;
+        const reader = response.body.getReader();
+        const decoder = new TextDecoder('utf-8');
+        let result = '';
+        let done = false;
+
+        while (!done) {
+          const { value, done: streamDone } = await reader.read();
+          done = streamDone;
+          result += decoder.decode(value, { stream: !done });
+        }
+        result = JSON.parse(result);
+        for (let i = 0; i < result.length; i++) {
+          result[i].Timestamp = new Date(result[i].Timestamp).toLocaleString();
+        }
+        setTableData(result);
+        console.log('Items:', result);
+      } catch (error) {
+        console.error('Error fetching items:', error);
+      }
+    };
+    fetchItems();
+  }, []);
+
+  React.useEffect(() => {
+    console.log('Table Data:', tableData);
+  }
+  , [tableData]);
+  
+
+
   if (!user.signInDetails) {
     return <div>Loading...</div>;
   }
@@ -34,6 +76,15 @@ function Home() {
       <ButtonAppBar />
       {console.log("User: ", user)}
       <p>Email: {user.signInDetails.loginId} </p>
+      {tableData.map((item) => (
+        <div key={item.PhoneNumber}>
+          <p>Name: {item.Name}</p>
+          <p>Phone Number: {item.PhoneNumber}</p>
+          <p>School: {item.School}</p>
+          <p>Leader: {item.Leader.toString()}</p>
+          <p>Timestamp: {item.Timestamp}</p>
+        </div>
+      ))}
     </div>
   );
 }
