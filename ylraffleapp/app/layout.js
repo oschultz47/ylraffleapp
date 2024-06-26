8f17cf4dfc7ec3c4d724cd48ad460eec2c4da2ec
oschultz47 <oschultz47@tamu.edu>
2024-06-26 12:54:28 -0500

    Add page title

--- ylraffleapp/app/layout.js
+++ ylraffleapp/app/layout.js
@@ -1,6 +1,11 @@
 import './globals.css';
 import { AuthProvider } from './context/AuthContext';
 
+export const metadata = {
+  title: 'BVYL Raffle',
+  description: 'Welcome to the BVYL Raffle application',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
